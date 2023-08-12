0966afc73963811a9adce98cbc4ea3ebe29ede55
Hossam <hossam.hassan.fcis@gmail.com>
2023-08-12 19:15:10 +0300

    make problems allow null values

--- database/migrations/2023_08_12_152919_add_student_problem_column.php
+++ database/migrations/2023_08_12_152919_add_student_problem_column.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('students', function (Blueprint $table) {
-            $table->string('problems')->after('gender')->default('');
+            $table->string('problems')->after('gender')->nullable(true)-> default('');
         });
     }
 
